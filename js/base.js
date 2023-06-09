4865476cf885c93dca4fc7525b59f66b885a3466
Nguyencoder11 <lenhnguyen10a22003@gmail.com>
2023-06-09 14:32:24 +0700

    new commit

---
+++ js/base.js
@@ -0,0 +1,50 @@
+const songs = [
+    {
+        name: "Tay Nắm Tay Rời",
+        link: "https://mp3-s1-zmp3.zmdcdn.me/f5657f66e6210f7f5630/1435179118580913627?authen=exp=1686466766~acl=/f5657f66e6210f7f5630/*~hmac=9a039444eacda3ac004d39a943587264&fs=MTY4NjI5Mzk2NjmUsIC3M3x3ZWJWNnwxMDY2OTExNDk0fDE0LjIyNC4xNDYdUngOTM",
+        artists: "Phạm Đình Thái Ngân",
+        image: "https://photo-resize-zmp3.zmdcdn.me/w240_r1x1_webp/cover/a/4/3/e/a43e2f55c5699c55e34703b8dc6aad3c.jpg"
+    },
+    {
+        name: "Cần 1 ai đó",
+        link: "https://mp3-s1-zmp3.zmdcdn.me/a53e06271a60f33eaa71/4307176343340663879?authen=exp=1686467124~acl=/a53e06271a60f33eaa71/*~hmac=fb5949076cb6bcc98e6f3ea6c90bcae3&fs=MTY4NjI5NDMyNDY3MXx3ZWJWNnwwfDExOC42OS4yLjI0Mw",
+        artists: "Phạm Đình Thái Ngân",
+        image: "https://photo-resize-zmp3.zmdcdn.me/w240_r1x1_webp/cover/a/5/3/8/a538d9c932eb45d4e713090821d55120.jpg"
+    },
+    {
+        name: "Hoa Cỏ Lau",
+        link: "https://mp3-s1-zmp3.zmdcdn.me/e793f595cdd624887dc7/2886723413343758544?authen=exp=1686466659~acl=/e793f595cdd624887dc7/*~hmac=25ad56bec9192dd7e6fb03e25a021b81&fs=MTY4NjI5Mzg1OTUwN3x3ZWJWNnwxMDIwMDA2MDI5fDExNS43NS4xMzgdUngMjM2",
+        artists: "Phong Max",
+        image: "https://photo-resize-zmp3.zmdcdn.me/w240_r1x1_webp/cover/5/2/2/3/522398904febc498797880c6af3ad3c3.jpg"
+    },
+    {
+        name: "Có Chàng Trai Viết Lên Cây",
+        link: "https://mp3-s1-zmp3.zmdcdn.me/3d63d46c0028e976b039/2675035284037628448?authen=exp=1686467035~acl=/3d63d46c0028e976b039/*~hmac=a4b2373eff15b18a9127187be8049dbe&fs=MTY4NjI5NDIzNTk0NHx3ZWJWNnwxMDA5Nzg2NTEyfDQyLjExMy4yMzkdUngMjAz",
+        artists: "Phan Mạnh Quỳnh",
+        image: "https://photo-resize-zmp3.zmdcdn.me/w240_r1x1_webp/covers/f/f/ffd96dc18d252825cb591e255ddc6dbd_1513860881.jpg"
+    },
+    {
+        name: "Đừng Lo Anh Đợi Mà",
+        link: "https://mp3-s1-zmp3.zmdcdn.me/f6512b763f31d66f8f20/4402714385343804339?authen=exp=1686466972~acl=/f6512b763f31d66f8f20/*~hmac=fb2807974397eb1ad5fcb2e44d509e06&fs=MTY4NjI5NDE3MjkyN3x3ZWJWNnwxMDExMzE2Mjg5fDExNS43Ni40Mi4xNDmUsIC",
+        artists: "Mr. Siro",
+        image: "https://photo-resize-zmp3.zmdcdn.me/w240_r1x1_webp/cover/f/1/c/0/f1c06ed70e6be6be9572d99daf5ecc53.jpg"
+    },
+    {
+        name: "Cuối Cùng Thì",
+        link: "https://mp3-s1-zmp3.zmdcdn.me/b1c58331cb71222f7b60/6895123281327191270?authen=exp=1686467291~acl=/b1c58331cb71222f7b60/*~hmac=26301268ddb3864cf02f083140d5afc3&fs=MTY4NjI5NDQ5MTmUsICxNnx3ZWJWNnwwfDExOC42OS4yLjI0Mw",
+        artists: "Jack",
+        image: "https://photo-resize-zmp3.zmdcdn.me/w240_r1x1_webp/cover/9/f/5/8/9f58cc8633750bb9a12bc153854a870a.jpg"
+    },
+    {
+        name: "Một Bước Yêu, Vạn Dặm Đau",
+        link: "https://mp3-s1-zmp3.zmdcdn.me/b9aa20ec13abfaf5a3ba/1502019886813006440?authen=exp=1686467010~acl=/b9aa20ec13abfaf5a3ba/*~hmac=b97bbb4e8e97c0c73de94b2b6b3e9f0d&fs=MTY4NjI5NDIxMDgwNXx3ZWJWNnwwfDEwMy4yMDYdUngMTkxLjM1",
+        artists: "Mr. Siro",
+        image: "https://photo-resize-zmp3.zmdcdn.me/w240_r1x1_webp/cover/4/d/6/2/4d62b014dddf2702af85e1d14d5c0a0b.jpg"
+    },
+    {
+        name: "Về Bên Anh",
+        link: "https://mp3-s1-zmp3.zmdcdn.me/00063ebaf6fe1fa046ef/2900534645056634121?authen=exp=1686467390~acl=/00063ebaf6fe1fa046ef/*~hmac=28faa9a80ba373ca3743628abe7f7ae8&fs=MTY4NjI5NDU5MDYwMXx3ZWJWNnwwfDExOC42OS4yLjI0Mw",
+        artists: "Jack",
+        image: "https://photo-resize-zmp3.zmdcdn.me/w240_r1x1_webp/cover/5/d/7/6/5d76f8f71c270b8e2adb413ae524037c.jpg"
+    }
+]
---
+++ js/main.js
@@ -0,0 +1,163 @@
+var progress = document.querySelector("#progress");
+var song = document.querySelector("#song");
+var playSong = document.querySelector(".zm-play");
+var pauseSong = document.querySelector(".zm-pause");
+
+var index = 0;
+var img = document.querySelector(".thumbnail img");
+
+var thumb = document.querySelector("#thumb");
+var title = document.querySelector("#title");
+var artist = document.querySelector("#artist");
+
+var start = document.querySelector("#start");
+var end = document.querySelector("#end");
+
+song.src = songs[index].link;
+title.innerHTML = songs[index].name;
+artist.innerHTML = songs[index].artists;
+thumb.src = songs[index].image;
+
+song.onloadedmetadata = function () {
+    progress.max = song.duration;
+    progress.value = song.currentTime;
+
+    setInterval(() => {
+        var min = Math.floor(song.duration / 60);
+        var sec = Math.floor(song.duration % 60);
+
+        var curMin = Math.floor(song.currentTime / 60);
+        var curSec = Math.floor(song.currentTime % 60);
+
+        if (sec < 10) {
+            sec = "0" + sec;
+        }
+        if (curSec < 10) {
+            curSec = "0" + curSec;
+        }
+        if (min < 10) {
+            min = "0" + min;
+        }
+        if (curMin < 10) {
+            curMin = "0" + curMin;
+        }
+
+        end.innerHTML = min + ":" + sec;
+        start.innerHTML = curMin + ":" + curSec;
+    }, 1000);
+};
+
+
+// Xu li su kien kich vao button play
+function playPause() {
+    // Khi click vao nut tam dung dang hien thi se thuc hien:
+    if (pauseSong.style.display == "block") {
+        song.pause();   // Dung bai hat
+        pauseSong.style.display = "none";   // An nut tam dung
+        playSong.style.display = "block";   // Hien thi nut choi nhac
+        img.classList.remove("play");   // Dung chuyen dong animation anh
+    } else {
+        song.play();    // Chay bai hat
+        pauseSong.style.display = "block";  // Hien thi nut tam dung
+        playSong.style.display = "none";    // An nut choi nhac
+        img.classList.add("play");  // Tiep tuc animation anh
+    }
+}
+
+// Khi click vao bat ki vi tri nao cua thanh tien do 
+// thoi gian se thay doi theo 
+if (song.play()) {
+    setInterval(() => {
+        progress.value = song.currentTime;
+        if (song.currentTime == song.duration) {
+            nextPlay();
+        }
+    }, 1000);
+}
+// dong thoi nut choi nhac hoac tam dung cung se thay doi
+progress.onchange = function () {
+    song.play();
+    song.currentTime = progress.value;
+    playSong.style.display = "none";
+    pauseSong.style.display = "block";
+    img.classList.add("play");
+};
+
+// Su kien backward and forward button
+function nextPlay() {
+    index = index + 1;
+    if (index > songs.length) {
+        index = 0;
+        song.src = songs[index].link;
+        title.innerHTML = songs[index].name;
+        artist.innerHTML = songs[index].artists;
+        thumb.src = songs[index].image;
+    }
+    else {
+        song.src = songs[index].link;
+        title.innerHTML = songs[index].name;
+        artist.innerHTML = songs[index].artists;
+        thumb.src = songs[index].image;
+    }
+    playSong.style.display = "block";
+    pauseSong.style.display = "none";
+    img.classList.remove("play");
+}
+function prevPlay() {
+    index = index - 1;
+    if (index < 0) {
+        index = songs.length;
+        song.src = songs[index].link;
+        title.innerHTML = songs[index].name;
+        artist.innerHTML = songs[index].artists;
+        thumb.src = songs[index].image;
+    } else {
+        song.src = songs[index].link;
+        title.innerHTML = songs[index].name;
+        artist.innerHTML = songs[index].artists;
+        thumb.src = songs[index].image;
+    }
+    playSong.style.display = "block";
+    pauseSong.style.display = "none";
+    img.classList.remove("play");
+}
+
+// Xu li su kien phat ngau nhien bai hat
+function shufflePlay() {
+    index = Math.floor(Math.random() * songs.length);
+    song.src = songs[index].link;
+    title.innerHTML = songs[index].name;
+    artist.innerHTML = songs[index].artists;
+    thumb.src = songs[index].image;
+    song.play();
+}
+
+// Xu li su kien lap lai bai hat
+function repeatSong() {
+    var showLoop = document.querySelector(".repeat-loop");
+    if (song.loop) {
+        song.loop = false;
+        song.removeAttribute("loop");
+        showLoop.style.display = "none";
+    } else {
+        song.loop = true;
+        song.setAttribute("loop", "loop");
+        showLoop.style.display = "block";
+    }
+}
+
+// Dieu chinh am luong
+var volumeSlider = document.querySelector("#btn-volume");
+volumeSlider.addEventListener("input", function () {
+    var curVol = volumeSlider.value;
+    song.volume = curVol;
+});
+
+// Cap nhat hien thi tien do bai hat
+var audio = new Howl({
+    src: songs[index].link,
+    onplay: function () {
+        var songDuration = audio.duration();
+        progress.max = songDuration;
+    }
+});
